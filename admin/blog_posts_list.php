778666da4783e67c7c67e7a7eb778fef1d745c91
GomiRyoichi <gomiryo@gmail.com>
2018-07-07 16:18:11 +0900

    ver 0.2.2

---
+++ admin/blog_posts_list.php
@@ -0,0 +1,9 @@
+<?php
+require("../global.php");
+require(MODEL_PATH. "admin/AdminBlogPostsModel.php");
+
+$obj = new AdminBlogPostsModel();
+// $obj->error_check();
+
+require(TEMPLATE_PATH. "admin/blog_posts_list.php");
+?>
