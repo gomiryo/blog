3824ee9335ba8e1c79468b5d6b21a0eaa36b3f3b
GomiRyoichi <gomiryo@gmail.com>
2018-07-10 13:40:33 +0900

    ver 0.3

--- private/model/admin/AdminBlogPostsModel.php
+++ private/model/admin/AdminBlogPostsModel.php
@@ -4,8 +4,37 @@ require_once(MODEL_PATH. "BaseModel.php");
 require_once(MODEL_PATH. "BlogPostsSqlModel.php");
 
 class AdminBlogPostsModel{
-
+  var $mode = "";
   function main(){
+    $this->mode = isset($_POST["mode"]) ? $_POST["mode"] : "none";
+
+    switch($this->mode){
+      case "new":
+        $this->setBlogPosts();
+        break;
+      case "del":
+        $this->delBlogPosts();
+        break;
+      case "upd":
+        $this->delBlogPosts();
+        break;
+      default:
+        return 0;
+    }
+  }
+
+  // 新規作成
+  function setBlogPosts(){
+    echo "setBlogPosts() start";
+    exit();
+  } 
+  // 削除
+  function delBlogPosts(){
+    echo "setBlogPosts() start";
+  }
+  // 更新
+  function updBlogPosts(){
+    echo "updBlogPosts() start";
   }
 
 }
